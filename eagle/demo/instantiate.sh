bbd80f8b29977aaf59a252aeeb7a264e33041e43
Mario Blunk <mario.blunk@blunk-electronic.de>
2020-04-16 17:20:19 +0200

    schematic instantiation complete

--- eagle/demo/instantiate.sh
+++ eagle/demo/instantiate.sh
@@ -36,25 +36,33 @@
 arg_count=$#
 input_module=$1
 prefix=$2
-index=$3
+index_in=$3
+index_out=$4
 
 extension_sch=".sch"
 extension_brd=".brd"
-
+separator="_"
 
 proc_test_arguments()
 	{
-	if [ $arg_count -lt 3 ]; then
+	if [ $arg_count -lt 4 ]; then
 		{
-		echo "ERROR: Provide argument for input module, prefix and index !"
-		echo "The input module must already have index '_1' like 'motor_driver_1.sch' ."
+		echo "ERROR: Provide argument for input module, prefix, input index, output index !"
+		#echo "The input module must already have index '_1' like 'motor_driver_1.sch' ."
 		exit 1
 		}
 	else
 		{
-		echo "input module:" $input_module
-		echo "prefix      :" $prefix
-		echo "index       :" $index
+		echo "input module   :" $input_module
+		echo "net prefix     :" $prefix   
+		echo "index in       :" $index_in
+		echo "index out      :" $index_out
+		
+		prefix_in_full=$prefix$separator$index_in
+		echo "prefix in full :" $prefix_in_full
+		
+		prefix_out_full=$prefix$separator$index_out
+		echo "prefix out full:" $prefix_out_full
 		}
 	fi
 	}
@@ -78,15 +86,18 @@ proc_copy()
 		generic_name=${input_module_simple_name:0:length-2}
 		#echo $generic_name
 		
-		# Compose the name of the new instantiated schematic file and create it.
-		# If it already exists then it will be overwritten without warning:
-		schematic_file=$generic_name"_"$index$extension_sch
-		#cp $input_module_simple_name$extension_sch $schematic_file
+		# Compose the name of the new instantiated schematic and board file.
+		# If a file already exists then it will be deleted and created anew 
+		# WITHOUT WARNING:
+		schematic_file=$generic_name"_"$index_out$extension_sch
+		if [ -e $schematic_file ]; then
+			rm $schematic_file
+		fi
 
-		# Compose the name of the new instantiated board file and create it.
-		# If it already exists then it will be overwritten without warning:
-		board_file=$generic_name"_"$index$extension_brd
-		#cp $input_module_simple_name$extension_brd $board_file		
+		board_file=$generic_name"_"$index_out$extension_brd
+		if [ -e $board_file ]; then
+			rm $board_file
+		fi
 		}
 	else
 		{
@@ -94,6 +105,12 @@ proc_copy()
 		}
 	fi
 	}
+	
+proc_line_not_changed()
+	{
+	echo $@ >> $schematic_file	
+	}
+	
 	
 proc_make_schematic()
 	{
@@ -103,7 +120,7 @@ proc_make_schematic()
 	trailer="class"
 # 	echo "header:" $header
 	
-	prefix_length=${#prefix}
+	prefix_length=${#prefix_in_full}
 # 	echo "prefix lenght:" $prefix_length
 # 	echo "renaming nets ..."
 	
@@ -118,10 +135,14 @@ proc_make_schematic()
 			l=${#net_name}
 			# echo $l
 			
+			# Extact the net name and the full trailer.
+			# The full trailer is something like "class="0" and will be used
+			# later when the new line is composed.
 			for i in $( eval echo {0..$l} ) ; do
 				if [ "${net_name:i:5}" = "$trailer" ]; then
+					trailer_full=${net_name:i}
 					net_name=${net_name:0:i-2}
-					break
+					break # no need for further parsing
 				fi
 			done
 			
@@ -129,12 +150,34 @@ proc_make_schematic()
 			
 			# The net name must be longer than the given prefix.
 			if [ "${#net_name}" -gt "$prefix_length" ]; then
-				echo "net" $net_name
+				#echo "net" $net_name # LED_DRV_1_IN
 				
-				net_name_base=${net_name:$prefix_length}
-				echo $net_name_base
+				# The net name must start with $prefix_in_full (like "LED_DRV_1")
+				if [ "${net_name:0:$prefix_length}" = "$prefix_in_full" ]; then
+					net_name_root=${net_name:$prefix_length}
+					#echo $net_name_root # _IN
+					
+					# The root name must start with a separator:
+					if [ "${net_name_root:0:1}" = "$separator" ]; then
+						#echo $net_name_root # _IN
+					
+						net_name_new=$prefix_out_full$net_name_root
+					
+						echo "renaming net" $net_name "to" $net_name_new
+						
+						# Compose the new line to be written in the output file:
+						echo $header"\""$net_name_new"\"" $trailer_full >> $schematic_file
+					else
+						proc_line_not_changed $line
+					fi
+				else
+					proc_line_not_changed $line
+				fi
+			else
+				proc_line_not_changed $line
 			fi
-			
+		else
+			proc_line_not_changed $line
 		fi
 			
 	done < $in_file
